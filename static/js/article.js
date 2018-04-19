0f003b58844dd51c934dcf9ba4b134027e0ab7c9
jingshuai <jingshuai-pbj@sinosig.com>
2018-04-19 20:05:11 +0800

    update

--- static/js/article.js
+++ static/js/article.js
@@ -1,4 +1,4 @@
 // 返回按钮
 document.getElementById('back').addEventListener('click', function() {
-    history.go(-1);
+    location.href = location.href.replace(/(.*)\/(static\/article)\/(\d+)\.html/, "$1" + "/index.html")
 })
